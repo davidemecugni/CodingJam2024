14f17dcd4187e18bd03394a69f8889c4452bfaac
Davide Mecugni <davidemecugni@gmail.com>
2024-03-22 20:05:00 +0100

    Da ottimizzare

--- code/2-Second/code.py
+++ code/2-Second/code.py
@@ -2,7 +2,8 @@
 # 1 pulcino) un numero dispari di cambi
 # 2 pulcini) 3 o 7 o 11 o 3+(4*n)
 # 3 pulcini) 7
-# 4 pulcini) o 47
+# 4 pulcini) 15 o 47
+# 5 pulcini) 2^n-1
 
 def n_dispari(n):
     return (n-1)*2 + 1
@@ -10,12 +11,17 @@ def n_dispari(n):
 def vede(n, k):
     if k == 0:
         return False
-    return (2**n - 1) % k == 0
+    j = 0
+    num = (2**n - 1)
+    while(num <= k):
+        if (num + 2**n * j) == k:
+            return True      
+        j += 1
+    return False
 
-    return k % n_dispari(n) == 0
 if __name__ == "__main__":
     #code/2-Second/input_2.txt
-    with open("code/2-Second/given.txt") as f: 
+    with open("code/2-Second/input_2.txt") as f: 
         s = f.read()
         s = s.split("\n")
     ss = []
